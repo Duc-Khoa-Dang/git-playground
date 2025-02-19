db8a4fa573d39dde53e334e77b5962c985b6906a
Duc Dang <dang@messkonzept.de>
2025-02-19 15:38:25 +0100

    Fix typo from log to lg

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ document.querySelectorAll('div').forEach(item => {
         item.style.color = "black"
     }
 })
+
+function exportAsPng(){
+    html2canvas(document.querySelector("body")).then(canvas => {
+        canvas.toBlob(function(blob){
+            let file = new Blob([blob], {type: "application/octet-stream"});
+            let blobURL = URL.createObjectURL(file);
+            window.location.href = blobURL;
+        })
+    });
+}
