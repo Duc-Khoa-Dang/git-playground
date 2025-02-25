af9416f9f5ce154f00b4c2ddd012078fd4f8c4e0
Duc Dang <dang@messkonzept.de>
2025-02-25 14:24:16 +0100

    Button functionality

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ document.querySelectorAll('div').forEach(item => {
 function exportAsPng(){
     html2canvas(document.querySelector("body")).then(canvas => {
         canvas.toBlob(function(blob){
+            saveAs(blob, "git-cheat-sheet.png")
             let file = new Blob([blob], {type: "application/octet-stream"});
             let blobURL = URL.createObjectURL(file);
             window.location.href = blobURL;
